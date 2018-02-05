22556c38253f8ff585a6560c6f182fe63ebac208
bodrul-work <bodrul.ic@msn.com>
2018-02-05 16:46:55 +0000

    Added some unit tests.

--- GumtreeScraper.UnitTests/RepositoryTests/CarMakeRespositoryTests.cs
+++ GumtreeScraper.UnitTests/RepositoryTests/CarMakeRespositoryTests.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Data.Entity.Infrastructure;
 using GumtreeScraper.Model;
 using GumtreeScraper.Repository;
 using NUnit.Framework;
@@ -28,6 +29,96 @@ namespace GumtreeScraper.UnitTests.RepositoryTests
         {
             // Create the same CarMake, should fail.
             Assert.Throws<ArgumentException>(CreateNewCarMake_CreateSuccessful_ReturnsPass);
+        }
+
+        [Test, Order(3)]
+        public void CreateNewCarModel_CreateSuccessful_ReturnsPass()
+        {
+            // Arrange.
+            CarMakeRepository carMakeRepo = new CarMakeRepository();
+            CarModelRepository carModelRepo = new CarModelRepository();
+            CarMake carMake = carMakeRepo.Get(x => x.Name.Equals("Renault"));
+            CarModel carModel = new CarModel();
+            carModel.CarMakeId = carMake.Id;
+            carModel.Name = "Clio";
+
+            // Act.
+            carModelRepo.Create(carModel);
+
+            // Assert.
+            Assert.Pass();
+        }
+
+        [Test, Order(4)]
+        public void CreateExistingCarModel_CreateFail_ReturnsPass()
+        {
+            // Create the same CarModel, should fail.
+            Assert.Throws<ArgumentException>(CreateNewCarModel_CreateSuccessful_ReturnsPass);
+        }
+
+        [Test, Order(5)]
+        public void UpdateCarMake_UpdateSuccessful_ReturnsPass()
+        {
+            // Arrange.
+            CarMakeRepository carMakeRepo = new CarMakeRepository();
+            CarMake carMake = carMakeRepo.Get(x => x.Name.Equals("Renault"));
+
+            // Act.
+            carMake.Name = "Ferrari";
+            carMakeRepo.Update(carMake);
+
+            // Assert.
+            Assert.Pass();
+        }
+
+        [Test, Order(6)]
+        public void UpdateCarModel_UpdateSuccessful_ReturnsPass()
+        {
+            // Arrange.
+            CarModelRepository carModelRepo = new CarModelRepository();
+            CarModel carModel = carModelRepo.Get(x => x.Name.Equals("Clio"));
+
+            // Act.
+            carModel.Name = "Enzo";
+            carModelRepo.Update(carModel);
+
+            // Assert.
+            Assert.Pass();
+        }
+
+        [Test, Order(7)]
+        public void DeleteCarMake_DeleteFail_ReturnsPass()
+        {
+            // Delete a CarMake when CarModels associated exist, should fail.
+            Assert.Throws<DbUpdateException>(DeleteCarMake_DeleteSuccessful_ReturnsPass);
+        }
+
+        [Test, Order(8)]
+        public void DeleteCarModel_DeleteFail_ReturnsPass()
+        {
+            // Arrange.
+            CarModelRepository carModelRepo = new CarModelRepository();
+            CarModel carModel = carModelRepo.Get(x => x.Name.Equals("Enzo"));
+
+            // Act.
+            carModelRepo.Delete(carModel);
+
+            // Assert.
+            Assert.Pass();
+        }
+
+        [Test, Order(9)]
+        public void DeleteCarMake_DeleteSuccessful_ReturnsPass()
+        {
+            // Arrange.
+            CarMakeRepository carMakeRepo = new CarMakeRepository();
+            CarMake carMake = carMakeRepo.Get(x => x.Name.Equals("Ferrari"));
+
+            // Act.
+            carMakeRepo.Delete(carMake);
+
+            // Assert.
+            Assert.Pass();
         }
     }
 }
