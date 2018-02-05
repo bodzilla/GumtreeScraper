7e5bf97e3057e1c7a6de4d603a43222e1f18ba17
bodrul <bodrul.ic@msn.com>
2018-02-05 23:06:39 +0000

    Added Article and ArticleVersion repos.

---
+++ GumtreeScraper.Repository/ArticleRepostiory.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using GumtreeScraper.Model;
+using GumtreeScraper.Repository.Interfaces;
+
+namespace GumtreeScraper.Repository
+{
+    public class ArticleRepository : IGenericRepository<Article>
+    {
+        private readonly GenericRepository<Article> _repository;
+
+        public ArticleRepository()
+        {
+            _repository = new GenericRepository<Article>();
+        }
+
+        public IList<Article> GetAll(params Expression<Func<Article, object>>[] navigationProperties)
+        {
+            return _repository.GetAll(x => x.VirtualArticleVersions, x => x.VirtualCarModel);
+        }
+
+        public IList<Article> GetList(Func<Article, bool> where, params Expression<Func<Article, object>>[] navigationProperties)
+        {
+            return _repository.GetList(where, navigationProperties);
+        }
+
+        public Article Get(Func<Article, bool> where, params Expression<Func<Article, object>>[] navigationProperties)
+        {
+            return _repository.Get(where, navigationProperties);
+        }
+
+        public bool Exists(Func<Article, bool> where)
+        {
+            return _repository.Exists(where);
+        }
+
+        public void Create(params Article[] articles)
+        {
+            // Check for duplicates before creating.
+            foreach (Article article in articles)
+            {
+                bool articleExists = Exists(x => x.Link == article.Link);
+                if (articleExists) throw new ArgumentException("Article exists.");
+            }
+            _repository.Create(articles);
+        }
+
+        public void Update(params Article[] articles)
+        {
+            // Check for duplicates before updating.
+            foreach (Article article in articles)
+            {
+                bool articleExists = Exists(x => x.Link == article.Link);
+                if (articleExists) throw new ArgumentException("Article exists.");
+            }
+            _repository.Update(articles);
+        }
+
+        public void Delete(params Article[] articles)
+        {
+            _repository.Delete(articles);
+        }
+    }
+}
---
+++ GumtreeScraper.Repository/ArticleVersionRepository.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using GumtreeScraper.Model;
+using GumtreeScraper.Repository.Interfaces;
+
+namespace GumtreeScraper.Repository
+{
+    public class ArticleVersionRepository : IGenericRepository<ArticleVersion>
+    {
+        private readonly GenericRepository<ArticleVersion> _repository;
+
+        public ArticleVersionRepository()
+        {
+            _repository = new GenericRepository<ArticleVersion>();
+        }
+
+        public IList<ArticleVersion> GetAll(params Expression<Func<ArticleVersion, object>>[] navigationProperties)
+        {
+            return _repository.GetAll(x => x.VirtualArticle);
+        }
+
+        public IList<ArticleVersion> GetList(Func<ArticleVersion, bool> where, params Expression<Func<ArticleVersion, object>>[] navigationProperties)
+        {
+            return _repository.GetList(where, navigationProperties);
+        }
+
+        public ArticleVersion Get(Func<ArticleVersion, bool> where, params Expression<Func<ArticleVersion, object>>[] navigationProperties)
+        {
+            return _repository.Get(where, navigationProperties);
+        }
+
+        public bool Exists(Func<ArticleVersion, bool> where)
+        {
+            return _repository.Exists(where);
+        }
+
+        public void Create(params ArticleVersion[] articleVersions)
+        {
+            // Check for duplicates before creating.
+            foreach (ArticleVersion articleVersion in articleVersions)
+            {
+                //bool articleExists = Exists(x => x.Title == articleVersion.);
+                //if (articleExists) throw new ArgumentException("Article exists.");
+            }
+            _repository.Create(articleVersions);
+        }
+
+        public void Update(params ArticleVersion[] articles)
+        {
+            // Check for duplicates before updating.
+            foreach (ArticleVersion article in articles)
+            {
+                //bool articleExists = Exists(x => x.Link == article.Link);
+                //if (articleExists) throw new ArgumentException("Article exists.");
+            }
+            _repository.Update(articles);
+        }
+
+        public void Delete(params ArticleVersion[] articles)
+        {
+            _repository.Delete(articles);
+        }
+    }
+}
