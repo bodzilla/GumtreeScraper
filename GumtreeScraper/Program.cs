62b3adb5a5ff2ec4ed2f6515217bcf5240410e00
bodrul <bodrul.ic@msn.com>
2018-02-12 19:30:08 +0000

    Code cleanup.

--- GumtreeScraper/Program.cs
+++ GumtreeScraper/Program.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.Configuration;
 using System.Linq;
-using System.Net;
 using System.Reflection;
 using System.Threading;
 using GumtreeScraper.Repository;
