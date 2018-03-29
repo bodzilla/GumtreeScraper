4a8dac6a718140a605104934a2672d588405e102
bodrul-work <bodrul.ic@msn.com>
2018-03-29 10:25:05 +0100

    Removed unused using.

--- GumtreeScraper/SearchListScraper.cs
+++ GumtreeScraper/SearchListScraper.cs
@@ -5,7 +5,6 @@ using System.Globalization;
 using System.IO;
 using System.Linq;
 using System.Net;
-using System.Net.Http;
 using System.Reflection;
 using System.Security.Cryptography;
 using System.Text;
